a2da513e83b625246a0963cdace061ab77b6b2a1
Carlos Paniagua <carlostellier@gmail.com>
2026-01-02 13:19:07 +0100

    debug push

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,29 @@
 self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  
+  let data = { title: 'Nueva oferta', body: 'Revisa la app para más detalles.', url: '/' };
+  
   if (event.data) {
-    const data = event.data.json();
-    
-    const options = {
-      body: data.body,
-      icon: '/icon-dark.png',
-      badge: '/icon-dark.png',
-      data: {
-        url: data.url || '/'
-      }
-    };
-
-    event.waitUntil(
-      self.registration.showNotification(data.title, options)
-    );
+    data = event.data.json();
   }
+
+  const options = {
+    body: data.body,
+    icon: '/icon-dark.png',
+    badge: '/icon-dark.png',
+    vibrate: [100, 50, 100],
+    data: {
+      url: data.url || '/'
+    },
+    // Importante para iOS y Chrome
+    tag: 'job-alert-' + Date.now(), 
+    renotify: true,
+    requireInteraction: true 
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(`PUSH: ${data.title}`, options)
+  );
 });
 
 self.addEventListener('notificationclick', function(event) {
